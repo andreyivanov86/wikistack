804b71e3329007acd7cd84d376abff19bf4cb24f
AndreyI <andreyivanov86@gmail.com>
2017-09-22 17:26:40 -0400

    Serever works on root

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const app = express();
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+// templating boilerplate setup
+// const env = nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
+// app.set('view engine', 'html'); // what file extension do our templates have
+// app.engine('html', nunjucks.render); // how to render html templates
 
 app.use(morgan('dev'));
 
-app.listen(3000, function(){
+const server = app.listen(3000, function(){
   console.log('Listening...')
 });
+
+app.use('/', routes);
