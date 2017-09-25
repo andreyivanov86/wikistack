32977336e38a8e57a17341bb55a7370bd7906c91
Rian Halperin <rianhalperin@gmail.com>
2017-09-25 10:26:26 -0400

    created db

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const path = require('path');
 
 // templating boilerplate setup
-// const env = nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
-// app.set('view engine', 'html'); // what file extension do our templates have
-// app.engine('html', nunjucks.render); // how to render html templates
+const env = nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
+app.set('view engine', 'html'); // what file extension do our templates have
+app.engine('html', nunjucks.render); // how to render html templates
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', routes);
 
