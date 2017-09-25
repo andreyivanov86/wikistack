074141ec698e130c887c8a44025a409c2c182744
Rian Halperin <rianhalperin@gmail.com>
2017-09-25 10:10:40 -0400

    added nunjucks templates

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const routes = require('./routes');
 // const env = nunjucks.configure('views', { noCache: true }); // where to find the views, caching off
 // app.set('view engine', 'html'); // what file extension do our templates have
 // app.engine('html', nunjucks.render); // how to render html templates
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 app.use('/', routes);
